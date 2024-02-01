ae6135694e88d31c15b8521d0bf80b1519f20eb3
Nkevin03 <knezosi6@gmail.com>
2024-02-01 22:08:43 +0100

    Final commit

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+"use client"
+import React, { useEffect, useRef } from 'react';
+import { Application } from '@splinetool/runtime';
+
+const SplineComponent = () => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    // Verifica che il canvas sia montato
+    if (canvasRef.current) {
+      const app = new Application(canvasRef.current);
+      app.load('https://prod.spline.design/Co6AxLBT5EUFsVSy/scene.splinecode');
+    }
+  }, []);
+
+  return <canvas ref={canvasRef} />;
+};
+
+export default SplineComponent;
+
